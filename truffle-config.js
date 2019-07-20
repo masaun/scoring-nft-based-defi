8ad5b4fd0b0988c33944c1a7c712ebc0d5f7b08d
masaun <masanoriuno@gmail.com>
2019-07-20 09:28:21 +0200

    Fix port from 7545 to 8545 in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -11,7 +11,8 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,     // Ganache-GUI
+      //port: 8545,   // Ganache-CLI
       network_id: "*",
     },
     ropsten: {

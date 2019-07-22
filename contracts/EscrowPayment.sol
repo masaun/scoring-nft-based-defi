1039a7d2a6a69b517f9c498e1a0c46cff6e5fabd
masaun <masanoriuno@gmail.com>
2019-07-22 09:05:04 +0200

    Use inherited Escrow.sol in Exscrowpayment.sol

--- contracts/EscrowPayment.sol
+++ contracts/EscrowPayment.sol
@@ -11,7 +11,25 @@ contract EscrowPayment is Escrow {
     {
         // in progress
     }
+
+
+    function escrowDepositsOf(address payee) public view returns (uint256) {
+        depositsOf(payee);
+    }
+
+    /**
+    * @dev Stores the sent amount as credit to be withdrawn.
+    * @param payee The destination address of the funds.
+    */
+    function escrowDeposit(address payee) public onlyPrimary payable {
+        deposit(payee);
+    }
+
+    /**
+    * @dev Withdraw accumulated balance for a payee.
+    * @param payee The address whose funds will be withdrawn and transferred to.
+    */
+    function escrowWithdraw(address payable payee) public onlyPrimary {
+        withdraw(payee);
+    }
 }
-
-
-

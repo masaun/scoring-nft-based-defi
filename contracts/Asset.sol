e5c8fff77ef76b7e388292f3f14d3e795b7b8c2f
masaun <masanoriuno@gmail.com>
2019-07-23 08:43:05 +0200

    Inherit TradeERC721Token.sol in Asset.sol

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -1,24 +1,23 @@
 pragma solidity ^0.5.0;
 
 /* @notice Using OpenZeppelin-solidity v2.1.1 */
-import "./openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
-import "./openzeppelin-solidity/contracts/payment/escrow/Escrow.sol";
+//import "./openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "./TradeERC721Token.sol";
 
 
-contract Asset is ERC721Full, Escrow {
+contract Asset is TradeERC721Token {
+
     constructor(
         string memory name,
         string memory symbol,
-        uint tokenId,
-        string memory tokenURI
+        address proxyRegistryAddress
     )
-        ERC721Full(name, symbol)
         public
+        TradeERC721Token(name, symbol, proxyRegistryAddress)  // Assign value to constructor of TradeERC721Token.sol
     {
-        _mint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        // in progress
     }
+
+
+    
 }
-
-
-
